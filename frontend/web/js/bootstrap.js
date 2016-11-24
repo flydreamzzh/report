83df5d75c38f7af33c46044a6a4606bf84c2712a
Administrator <Administrator@PC-20161029KEGY.lan>
2016-11-24 12:49:44 +0800

    测试

--- frontend/web/js/bootstrap.js
+++ frontend/web/js/bootstrap.js
@@ -6,15 +6,18 @@ require.config({
         'angular-async-loader': 'lib/angular-async-loader/angular-async-loader.min',
         'angular-ui-mask': 'lib/angular-ui-mask/dist/mask.min',
         'ng-tags-input': 'lib/ng-tags-input/build/ng-tags-input.min',
-        'ng-file-upload': 'lib/ng-file-upload/dist/ng-file-upload-all.min'
+        'ng-file-upload': 'lib/ng-file-upload/dist/ng-file-upload-all.min',
+        'angular-strap': 'lib/angular-strap/dist/angular-strap.min',
+        'angular-strap-tpl': 'lib/angular-strap/dist/angular-strap.tpl.min',
     },
     shim: {
         'angular': {exports: 'angular'},
-        'angular-ui-router': {deps: ['angular']}
+        'angular-ui-router': {deps: ['angular']},
+        'angular-strap':{deps: ['angular','angular-strap-tpl']},
     }
 });
 
-require(['angular', './app-routes'], function (angular) {
+require(['angular', 'angular-strap','./app-routes'], function (angular) {
     angular.element(document).ready(function () {
         angular.bootstrap(document, ['app']);
         angular.element(document).find('html').addClass('ng-app');
