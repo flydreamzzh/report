9baabe4df56941148cf227bd4edd28cffe17e2d8
Administrator <Administrator@PC-20161029KEGY>
2016-11-26 17:29:02 +0800

    测试

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
+    <link rel="shortcut icon" href="/images/icon.png" type="image/x-icon">
     <title>REPORT</title>
     <?php $this->head() ?>
 </head>
