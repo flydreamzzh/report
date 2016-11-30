f81b830d25fb8f2a867c83d9f3b3777266127017
Administrator <Administrator@PC-20161029KEGY>
2016-11-30 12:00:23 +0800

    测试

--- frontend/web/js/scripts/controller/home/homeCtrl.js
+++ frontend/web/js/scripts/controller/home/homeCtrl.js
@@ -1,8 +1,21 @@
 define(['app','css!../../../../css/home/home'],function (app) {
-    app.controller('homeCtrl', ['$scope', function($scope) {
-    	$scope.modal = {
-    			"title": "Title",
-    			"content": "Hello Modal <br/>This is a multiline message!"
-    	};
-    }]);
+    app.controller('homeCtrl', function($scope) {
+    	$scope.user = {username:'',userpass:''};
+		$scope.checkuser = function(){
+			if(!$scope.user.username) {
+				$scope.matchuser = 0;
+			} else if($scope.user.username.match(/[a-zA-Z0-9]{5,15}/)) {
+				$scope.matchuser = 1;
+			} else {
+				$scope.matchuser = 2;
+			}
+		}
+		$scope.checkpass = function(){
+			if(!$scope.user.userpass) {
+				$scope.matchpass = 0;
+			} else {
+				$scope.matchpass = 1;
+			}
+		}
+    });
 });
