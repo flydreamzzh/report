97e9edf43674be6870afac04bd9549817fc2fd93
Administrator <Administrator@PC-20161029KEGY>
2016-12-10 18:03:07 +0800

    测试

--- frontend/web/js/scripts/controller/layout/layoutCtrl.js
+++ frontend/web/js/scripts/controller/layout/layoutCtrl.js
@@ -5,4 +5,14 @@ define(['app','css!../../../../css/main/layout/layout'],function(app){
     app.controller('layoutCtrl',function($scope){
 
     });
+
+    app.controller('userLoginCtrl',function($scope){
+        //$scope.show = function() {
+        //    $scope.bgShow = true;
+        //
+        //}
+        //$scope.hide = function() {
+        //    $scope.bgShow = false;
+        //}
+    })
 })
