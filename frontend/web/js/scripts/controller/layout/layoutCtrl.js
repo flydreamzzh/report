90e61732372e4d8daaed8ac4d8590b2815a8054c
Administrator <Administrator@PC-20161029KEGY>
2016-12-12 18:11:16 +0800

    测试

--- frontend/web/js/scripts/controller/layout/layoutCtrl.js
+++ frontend/web/js/scripts/controller/layout/layoutCtrl.js
@@ -7,12 +7,14 @@ define(['app','css!../../../../css/main/layout/layout'],function(app){
     });
 
     app.controller('userLoginCtrl',function($scope){
-        //$scope.show = function() {
-        //    $scope.bgShow = true;
-        //
-        //}
-        //$scope.hide = function() {
-        //    $scope.bgShow = false;
-        //}
-    })
+        $scope.ngShow = function(){
+            $scope.myCheck = true;
+            $scope.myCheck = true;
+            angular.element('.userLoginBg').addClass('bdfade');
+        }
+        $scope.ngHide = function(){
+            $scope.myCheck = false;
+            angular.element('.userLoginBg').removeClass('bdfade');
+        }
+    });
 })
